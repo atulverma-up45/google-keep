02023849e08014d9e35eeeeca0703c713dd7ba4e
AtulVerma_up45 <AtulVerma04680@gmail.com>
2024-01-05 22:06:14 +0530

    database config

---
+++ config/database.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv/config";
+
+const DB_NAME = "GoogleKeep"; 
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`MongoDB connected!! DB Host ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
